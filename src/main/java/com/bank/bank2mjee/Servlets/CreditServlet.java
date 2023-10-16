c4fc686365b6979117fd6fa99ac71bdf9abd72cf
MAROUANE <marwaneaitelhaj001@gmail.com>
2023-10-16 11:52:37 +0100

    update

---
+++ src/main/java/com/bank/bank2mjee/Servlets/CreditServlet.java
@@ -0,0 +1,32 @@
+package com.bank.bank2mjee.Servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/credit/create", "/credit/update"})
+public class CreditServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        switch (req.getRequestURI()) {
+            case "/credit/create":
+                this.create(req,resp);
+                break;
+            case "/credit/update":
+                break;
+        }
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+    private void create(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/Views/Credit/simulation.jsp").forward(req,resp);
+    }
+}
