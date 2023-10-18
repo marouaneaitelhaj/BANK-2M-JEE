4d2960c1cf6fba8f93d4d6c825c1ea3f72b8d204
Mohammed Moustarhfir <112891663+Mousta-Med@users.noreply.github.com>
2023-10-18 09:56:25 +0100

    client front

--- src/main/java/com/bank/bank2mjee/Services/ClientService.java
+++ src/main/java/com/bank/bank2mjee/Services/ClientService.java
@@ -8,21 +8,30 @@ import java.util.Optional;
 
 public class ClientService {
     private final ClientDao clientDao;
+
     public ClientService(ClientDao clientDao) {
         this.clientDao = clientDao;
     }
-    public Optional<Client> addClient(Client client){
+
+    public Optional<Client> addClient(Client client) {
         return clientDao.save(client);
     }
-    public Optional<Client> updateClient(Client client, String code){
+
+    public Optional<Client> updateClient(Client client, String code) {
         return clientDao.update(client, code);
     }
+
     public Integer deleteClient(String code) {
         return clientDao.delete(code);
     }
+
     public Optional<Client> getClient(String code) {
-       return clientDao.findOne(code);
+        return clientDao.findOne(code);
     }
+    public Optional<Client> getClientByName(String name) {
+        return clientDao.findByName(name);
+    }
+
     public List<Client> getClients() {
         return clientDao.findAll();
     }
--- src/main/java/com/bank/bank2mjee/Servlets/ClientServlet.java
+++ src/main/java/com/bank/bank2mjee/Servlets/ClientServlet.java
@@ -13,18 +13,22 @@ import jakarta.servlet.http.*;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
-@WebServlet(name = "ClientServlet", urlPatterns = {"/clients","/clientAdd", "/clientDelete"})
+@WebServlet(name = "ClientServlet", urlPatterns = {"/clients", "/client", "/clientAdd", "/clientDelete", "/clientUpdate"})
 public class ClientServlet extends HttpServlet {
     ClientDao clientDao = new ClientDaoImpl();
     ClientService clientService = new ClientService(clientDao);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         switch (request.getRequestURI()) {
             case "/clients":
+                getClients(request, response);
                 break;
-            case "/clientAdd":
+            default:
+                getClients(request, response);
                 break;
         }
     }
@@ -32,11 +36,24 @@ public class ClientServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         switch (request.getRequestURI()) {
+            case "/client":
+                getClient(request, response);
+                break;
             case "/clientAdd":
                 addClient(request, response);
                 break;
+            case "/clientDelete":
+                deleteClient(request, response);
+                break;
+            case "/clientUpdate":
+                updateClient(request, response);
+                break;
+            default:
+                getClients(request, response);
+                break;
         }
     }
+
     protected void addClient(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String code = request.getParameter("code");
         String firstName = request.getParameter("firstName");
@@ -53,6 +70,52 @@ public class ClientServlet extends HttpServlet {
             message = "Client Not Inserted";
         }
         request.setAttribute("message", message);
-//        getClients(request, response);
+        getClients(request, response);
     }
+
+    protected void getClient(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String code = request.getParameter("code");
+        Optional<Client> optionalClient = clientService.getClient(code);
+        if (optionalClient.isPresent())
+            request.setAttribute("client", optionalClient.get());
+        request.getRequestDispatcher("/Views/Client/client.jsp").forward(request, response);
+    }
+
+    protected void getClients(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Client> clients = clientService.getClients();
+        request.setAttribute("clients", clients);
+        request.getRequestDispatcher("/Views/Client/client.jsp").forward(request, response);
+    }
+    private void deleteClient(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String code = request.getParameter("code");
+        Integer res = clientService.deleteClient(code);
+        String message = null;
+        if (res != 0) {
+            message = "Client Deleted Successfuly";
+        } else {
+            message = "Client Not Deleted";
+        }
+        request.setAttribute("message", message);
+        getClients(request, response);
+    }
+
+    private void updateClient(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String code = request.getParameter("code");
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        LocalDate birthDate = LocalDate.parse(request.getParameter("birthDate"));
+        String mobile = request.getParameter("mobile");
+        String address = request.getParameter("address");
+        Client client = new Client(firstName, lastName, birthDate, mobile, code, address);
+        Optional<Client> optionalClient = clientService.updateClient(client, code);
+        String message = null;
+        if (optionalClient.isPresent()) {
+            message = "Client Updated Successfuly";
+        } else {
+            message = "Client Not Updated ";
+        }
+        request.setAttribute("message", message);
+        getClients(request, response);
+    }
+
 }
