e20566d4315c50df1c3955ab6da88a40ae794047
Mohammed Moustarhfir <112891663+Mousta-Med@users.noreply.github.com>
2023-10-23 00:12:04 +0100

    reformate code

--- src/main/webapp/Views/js/script.js
+++ src/main/webapp/Views/js/script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const alert = document.getElementById('alert');
     const closeAlert = document.getElementById('closeAlert');
 
-    openAddForm.addEventListener('click', function (){
+    openAddForm.addEventListener('click', function () {
         addForm.classList.remove('hidden');
     });
     addOverlay.addEventListener('click', function (event) {
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
             addForm.classList.add('hidden');
         }
     });
-    closeAddForm.addEventListener('click', function (){
+    closeAddForm.addEventListener('click', function () {
         addForm.classList.add('hidden');
     });
     deleteOverlay.addEventListener('click', function (event) {
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById('deleteForm').classList.add('hidden');
         }
     });
-    closeDeleteForm.addEventListener('click', function (){
+    closeDeleteForm.addEventListener('click', function () {
         document.getElementById('deleteForm').classList.add('hidden');
     });
 
@@ -35,16 +35,16 @@ document.addEventListener('DOMContentLoaded', function () {
             document.getElementById('updateForm').classList.add('hidden');
         }
     });
-    closeUpdateForm.addEventListener('click', function (){
+    closeUpdateForm.addEventListener('click', function () {
         document.getElementById('updateForm').classList.add('hidden');
     });
     if (alert) {
         setTimeout(function () {
             alert.style.display = 'none';
         }, 4000);
-    closeAlert.addEventListener('click', function () {
-        alert.classList.add('hidden');
-    });
+        closeAlert.addEventListener('click', function () {
+            alert.classList.add('hidden');
+        });
     }
 });
 function populateDelete(code) {
